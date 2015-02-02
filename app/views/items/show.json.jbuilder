6a97db79f1c993936019159812efb3955fc65ba6
Jesseract <jlpearce@gmail.com>
2015-02-02 16:13:39 -0500

    Create project and tables

---
+++ app/views/items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @bank_register, :id, :transaction_name, :transaction_amount, :total, :created_at, :updated_at
---
+++ db/migrate/20150202211003_add_decimal_places.rb
@@ -0,0 +1,5 @@
+class AddDecimalPlaces < ActiveRecord::Migration
+  def change
+    change_column :items, :price, :decimal, precision: 10, scale: 2
+  end
+end
