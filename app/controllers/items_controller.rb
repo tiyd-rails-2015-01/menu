aae381c5f2dbb664e827e9e8f5e2bff9c5f759ce
Topstitch <kallen@spoonflower.com>
2015-02-02 17:42:49 -0500

    Most things work

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,65 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :destroy]
+
+  # GET /foods
+  # GET /foods.json
+  def index
+    @items = Item.all
+  end
+
+  # GET /foods/new
+  def new
+    @item = Item.new
+  end
+
+  # POST /foods
+  # POST /foods.json
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to items_path, notice: 'Item was successfully created.' }
+        # format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new }
+        # format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /foods/1
+  # PATCH/PUT /foods/1.json
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to items_path, notice: 'Item was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        # format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /foods/1
+  # DELETE /foods/1.json
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_path, notice: 'Item was successfully destroyed.' }
+      # format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :section_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ActiveRecord::Base
-  belongs_to :sections
+  belongs_to :section
 end
