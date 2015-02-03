34d73791591569938dc9bf667a3eefa048e4c27a
Ashley Ellis <anellis12@gmail.com>
2015-02-03 12:34:41 -0500

    Fix create bug by allowing category_id as a parameter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,6 @@ class ItemsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def item_params
-    params.require(:item).permit(:name, :price, :description, :category_name)
+    params.require(:item).permit(:name, :price, :description, :category_id)
   end
 end
