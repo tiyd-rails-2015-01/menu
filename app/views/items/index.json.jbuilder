40e9a8875f5ebc87b94759d8e29c911bfc7c7351
C Haden <chaden@spoonflower.com>
2015-02-03 16:19:35 -0500

    add delete link

--- app/views/items/index.json.jbuilder
+++ app/views/items/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@items) do |item|
-  json.extract! item, :id, :image, :price, :name, :description, :category_key
+  json.extract! item, :id, :price, :name, :description, :category_key
   json.url item_url(item, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202183845) do
+ActiveRecord::Schema.define(version: 20150203204500) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "description"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20150202183845) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string   "image"
     t.float    "price"
     t.string   "description"
     t.integer  "category_key"
