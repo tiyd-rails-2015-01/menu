0ac1d0f92db5a5721518b3fca62763fbc7738648
Mike Marbry <mlmarbry@gmail.com>
2015-02-03 09:21:18 -0500

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :foods
   get 'categories/index'
+  get 'foods/new'
+  get 'foods/edit'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
