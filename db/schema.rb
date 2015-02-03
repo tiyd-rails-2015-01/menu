c38bf31d85ead10821fe4ae3f314373cb3867de8
Chris Foster <fosterchris22@gmail.com>
2015-02-03 14:29:15 -0500

    Add meal_id column to Categories.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202183900) do
+ActiveRecord::Schema.define(version: 20150203192805) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "meal_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -25,6 +26,12 @@ ActiveRecord::Schema.define(version: 20150202183900) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
