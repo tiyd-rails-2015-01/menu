30f6e07edb51202fea98ea9d743a83670e810b83
Jesseract <jlpearce@gmail.com>
2015-02-02 21:41:18 -0500

    Get menu to sort, pictures and other bugs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,77 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
-    @items = Item.all
+    @item = Item.all
+    @entree_items = Item.all.where(category_id: 2)
+    @app_items = Item.all.where(category_id: 1)
+    @special_items = Item.all.where(category_id: 3)
   end
 
+  def show
+    # @item = Item.find(params[:id])
+  end
+
+  # GET /bank_registers/new
+  def new
+    @item = Item.new
+  end
+
+  # GET /bank_registers/1/edit
+  def edit
+    # @item = Item.find(params[:id])
+  end
+
+  # POST /bank_registers
+  # POST /bank_registers.json
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /bank_registers/1
+  # PATCH/PUT /bank_registers/1.json
+  def update
+    # @item = Item.find(params[:id])
+
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to @item, notice: "Item was successfully updated." }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /bank_registers/1
+  # DELETE /bank_registers/1.json
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category)
+  end
 end
