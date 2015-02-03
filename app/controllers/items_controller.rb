e8891c7e27ba51fe67e1849308d2ef1bbc453a76
Ashley Ellis <anellis12@gmail.com>
2015-02-03 11:26:27 -0500

    Refactor to make code simpler

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,49 +13,29 @@ class ItemsController < ApplicationController
   end
 
   def index
-    Item.categorize
-    @entrees = Category.where(name: "Entree").first.items
-    @desserts = Category.where(name: "Dessert").first.items
-    @appetizers = Category.where(name: "Appetizers").first.items
-    @entree = Category.where(name: "Entree").first.name
-    @appetizer=Category.where(name: "Appetizers").first.name
-    @dessert=Category.where(name: "Dessert").first.name
-
+    @categories= Category.all
   end
 
   def create
     @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item was successfully updated.'
+    else
+      render :edit
     end
-
   end
 
   def destroy
     @item.destroy
-    respond_to do |format|
-      format.html { redirect_to item_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to item_url, notice: 'Item was successfully destroyed.'
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,3 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-
-  def self.categorize
-    if self.count> 0
-      Item.all.map do |item|
-          category = Category.where(name: item.category_name).first
-          category.items << item
-      end
-    end
-  end
 end
