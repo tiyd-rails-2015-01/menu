f81832f8dd84a214a1b4530f45fe48dc5a41171c
Owen <owenrham@gmail.com>
2015-02-02 22:34:18 -0500

    Add picture column to course table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202184943) do
+ActiveRecord::Schema.define(version: 20150203032114) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "course_type"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "picture"
   end
 
   create_table "items", force: :cascade do |t|
