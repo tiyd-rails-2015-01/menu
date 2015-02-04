86db44b438d9a24a49537b70e6a14d5f90abf298
Ruben <ssrcruz@gmail.com>
2015-02-03 20:59:12 -0500

    Add Images to categories. #shipit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,8 @@ class CategoriesController < ApplicationController
   def home
     @items = Item.all
     @categories = Category.all
-
+    @entrees = Item.all.where(category_id: 1)
+    @desserts = Item.all.where(category_id: 2)
   end
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203192523) do
+ActiveRecord::Schema.define(version: 20150203223112) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "image"
   end
 
   create_table "items", force: :cascade do |t|
