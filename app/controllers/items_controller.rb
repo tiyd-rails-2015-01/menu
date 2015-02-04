ade9acddc9aa0ccffa14bbb5dff15c71e782284a
Topstitch <kallen@spoonflower.com>
2015-02-04 14:59:28 -0500

    Attempt to solve font issue.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_meal, except: [:index]
 
   def index
     @items = Item.all
+    # load_my_selection  do this instead of before action
   end
 
   def new
@@ -36,6 +38,11 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_meal
+    session[:item] = Item.find(params[:id]).name
+    # redirect_to items_path   index doesn't have an id, it's a collection, would blow up if it wasn't except index
+  end
 
   def set_item
     @item = Item.find(params[:id])
