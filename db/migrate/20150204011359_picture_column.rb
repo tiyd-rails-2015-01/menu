e583098a99c43686a05b784f7545c5e88c1aedc3
Parker Ennis <rpennis@ncsu.edu>
2015-02-03 21:42:25 -0500

    working minus edit item links

---
+++ db/migrate/20150204011359_picture_column.rb
@@ -0,0 +1,5 @@
+class PictureColumn < ActiveRecord::Migration
+  def change
+    add_column :courses, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202190907) do
+ActiveRecord::Schema.define(version: 20150204011359) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "items", force: :cascade do |t|
